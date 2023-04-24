853a273890a305aa61ffda9c3adf8f5c06562750
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-04-24 21:49:12 +0300

    Product details implemented

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import useFetch from '../hooks/useFetch';
+import RelatedProducts from '../components/RelatedProducts';
+import { CartContext } from '../context/CartContext';
 
 const ProductDetails = () => {
-  return (
-    <div>ProductDetails</div>
-  )
-}
+  const { id } = useParams();
+  const { data } = useFetch(`/products?populate=*&filters[id][$eq]=${id}`);
 
-export default ProductDetails
+  const product = data?.[0].attributes;
+
+  if (!product) return <div className="container mx-auto">Loading...</div>;
+
+  console.log(product);
+  return (
+    <div className="mb-16 pt-44 lg:pt-[30px] xl:pt-0">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row gap-[30px] mb-[30px]">
+          <div
+            className="flex-1 lg:max-w-[40%] lg:h-[540px] grad rounded-lg
+          flex justify-center items-center"
+          >
+            <img
+              src={`${process.env.REACT_APP_API_URL}${product?.image?.data?.attributes?.url}`}
+              alt=""
+              className="w-full max-w-[65%]"
+            />
+          </div>
+          <div
+            className="flex-1 bg-primary p-12 xl:p-20 rounded-xl flex flex-col
+          justify-center"
+          >
+            <div className="uppercase text-accent text-lg font-medium mb-2">
+              {product.categories?.data[0]?.attributes?.title} cameras
+            </div>
+            <h2 className="h2 mb-4">{product.title}</h2>
+            <p className="mb-12">{product.description}</p>
+            <div className="flex items-center gap-x-8">
+              <div className="text-3xl text-accent font-semibold">
+                ${product.price}
+              </div>
+              <button className="btn btn-accent">Add to cart</button>
+            </div>
+          </div>
+        </div>
+        <RelatedProducts />
+      </div>
+    </div>
+  );
+};
+
+export default ProductDetails;
